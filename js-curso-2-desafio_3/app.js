0fbc3cd3666c466afb2fb7d6eb9d6a9bc134a51f
G. Kelly <38537574+kelly-u@users.noreply.github.com>
2024-08-15 17:14:00 -0300

    Inicio do desafio 3 - ainda incompleto

---
+++ js-curso-2-desafio_3/app.js
@@ -0,0 +1,20 @@
+// Crie uma função que calcule o índice de massa corporal (IMC) de uma pessoa, a partir de sua altura, em metros, e peso, em quilogramas, que serão recebidos como parâmetro.
+function calcularImc(altura, peso){
+    let imc = (altura * altura) / peso;
+    console.log(imc);
+}
+
+calcularImc(1.60, 90);
+
+/***********************************************************/
+
+// Crie uma função que calcule o valor do fatorial de um número passado como parâmetro.
+function calcularFatorial(numero){
+    let resultadoFatorial = 0;
+    while(numero > 0){
+        
+    }
+    console.log(resultadoFatorial);
+}
+
+calcularFatorial(3);
